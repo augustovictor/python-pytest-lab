41894928fa580baf4fe730c1139deb6c9aba3ab2
Victor Augusto <victoraweb@gmail.com>
2018-06-04 22:38:48 -0300

    [FEAT] Add generic operation

--- calc.py
+++ calc.py
@@ -1,2 +1,13 @@
 def add(a, b):
     return a + b
+
+def sub(a, b):
+    return a - b
+
+def genericCalc(a, b, operationType):
+    if operationType == 'sum':
+        return add(a, b)
+    elif operationType == 'sub':
+        return sub(a, b)
+    else:
+        raise ValueError("The values must be either 'add' or 'sub'")
