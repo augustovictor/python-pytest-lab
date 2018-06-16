5fd688e7d6985efb6c22251ebb949beb1790813f
Victor Augusto <victoraweb@gmail.com>
2018-06-15 23:05:18 -0300

    [TEST] Test json file loaded by conftest

---
+++ tests/test_authors.py
@@ -0,0 +1,15 @@
+import pytest
+import json
+
+def test_author_1_from_boston(author_file_json):
+    with author_file_json.open() as f:
+        authors = json.load(f)
+        assert authors['Author1']['city'] == 'Boston'
+
+def test_all_have_city_attribute(author_file_json):
+    with author_file_json.open() as f:
+        authors = json.load(f)
+
+        for a in authors:
+            print(authors[a])
+            assert len(authors[a]['city']) > 0
